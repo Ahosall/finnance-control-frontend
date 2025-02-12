7b0ec7f061ffc42884d532cd91c6e90978d6fdab
Ahosall <zen.wixyos.br@gmail.com>
2025-02-12 19:57:06 -0400

    improve data organization of listCategoriesForDashboard function and add new property in category interface

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,6 +14,7 @@ export interface ICategory {
   name: string;
   total: number;
   type: "EXPENSE" | "INCOME";
+  updatedAt: number;
 }
 
 type TCategoriesForDashboardFunction = () => Promise<ICategory[]>;
@@ -21,37 +22,52 @@ type TCategoriesForDashboardFunction = () => Promise<ICategory[]>;
 export const listCategoriesForDashboard: TCategoriesForDashboardFunction =
   async () => {
     return new Promise((resolve) => {
+      const transactions: ICategory[] = [
+        {
+          id: "a",
+          name: "Salário",
+          total: Math.random() * 3000,
+          type: "INCOME",
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
+        },
+        {
+          id: "d",
+          name: "Despesas Obrigatórias",
+          total: Math.random() * 1000,
+          type: "EXPENSE",
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+        },
+        {
+          id: "b",
+          name: "Outras Receitas",
+          total: Math.random() * 50,
+          type: "INCOME",
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+        },
+        {
+          id: "c",
+          name: "Despesas Variáveis",
+          total: Math.random() * 500,
+          type: "EXPENSE",
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
+        },
+      ];
+
+      transactions.sort((a, b) => {
+        if (a.type !== b.type) {
+          return a.type === "INCOME" ? -1 : 1;
+        }
+
+        // Se forem do mesmo tipo, ordena por data (mais recente primeiro)
+        return a.updatedAt - b.updatedAt;
+      });
+
       setTimeout(() => {
-        resolve([
-          {
-            id: "a",
-            name: "Salário",
-            total: Math.random() * 3000,
-            type: "INCOME",
-          },
-          {
-            id: "b",
-            name: "Doações",
-            total: Math.random() * 50,
-            type: "INCOME",
-          },
-          {
-            id: "c",
-            name: "Despesas Variáveis",
-            total: Math.random() * 500,
-            type: "EXPENSE",
-          },
-          {
-            id: "d",
-            name: "Despesas Obrigatórias",
-            total: Math.random() * 1000,
-            type: "EXPENSE",
-          },
-        ]);
+        resolve(transactions);
       }, 300);
     });
   };
 
-export const createTransaction = (data: ITransaction) => {};
+export const createTransaction = async (data: ITransaction) => {};
 
-export const editTransaction = (id: string, data: ITransaction) => {};
+export const editTransaction = async (id: string, data: ITransaction) => {};
