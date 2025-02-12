c94a99b8e0063c77ef6d4af0ec1aba01cd3ae1e1
Ahosall <zen.wixyos.br@gmail.com>
2025-02-12 19:58:22 -0400

    load category type in component to display transaction type icon

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,17 +1,33 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Grid2 as Grid, Typography } from "@mui/material";
+
+import {
+  PaidOutlined as IncomeIcon,
+  ReceiptLongOutlined as ExpenseIcon,
+} from "@mui/icons-material";
 
 interface Props {
   name: string;
   total: number;
+  type: "INCOME" | "EXPENSE";
 }
 
-const Category = ({ name, total }: Props) => {
+const Category = ({ name, total, type }: Props) => {
   return (
     <Card sx={{ width: "100%" }}>
       <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          {name}
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid size={2}>
+            {type === "INCOME" ? <IncomeIcon color="success" /> : <ExpenseIcon color="error"/>}
+          </Grid>
+          <Grid size="auto">
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14 }}
+            >
+              {name}
+            </Typography>
+          </Grid>
+        </Grid>
         <Typography sx={{ fontWeight: 500, fontSize: 32 }} component="div">
           {new Intl.NumberFormat("pt-BR", {
             style: "currency",
