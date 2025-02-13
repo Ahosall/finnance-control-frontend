71855f3e0ee94aab91a2bee894e9151cb78868c1
Ahosall <zen.wixyos.br@gmail.com>
2025-02-13 11:10:28 -0400

    organize api data

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,9 @@
  * API Area
  */
 
+// Types
 export interface ITransaction {
+  id: string;
   date: Date;
   categoryId: string;
   amount: number;
@@ -16,48 +18,115 @@ export interface ICategory {
   updatedAt: number;
 }
 
-export type TCategoryForDashboard = {
-  total: number;
-} & ICategory;
+export type TCategoryForDashboard = { total: number } & ICategory;
 
 type TListTransactionsFunction = () => Promise<ITransaction[]>;
+type TGetTransactionFunction = (id: string) => Promise<ITransaction | null>;
+
 type TListCategoriesFunction = () => Promise<ICategory[]>;
 type TListCategoriesDashboardFunction = () => Promise<TCategoryForDashboard[]>;
 
-export const listTransactions: TListTransactionsFunction = async () => {
-  const transactions: ITransaction[] = [
-    {
-      date: new Date("2025-02-12T12:00:00"),
-      description: "Compras diversas",
-      amount: 210,
-      categoryId: "c",
-    },
-    {
-      date: new Date("2025-02-11T12:00:00"),
-      description: "Pgto. Aluguel Casa",
-      amount: 2600,
-      categoryId: "d",
-    },
-    {
-      date: new Date("2025-02-09T12:00:00"),
-      description: "Lucro com ações",
-      amount: 480,
-      categoryId: "b",
-    },
-    {
-      date: new Date("2025-02-08T12:00:00"),
-      description: "Pgto. Agua e Luz",
-      amount: 370,
-      categoryId: "d",
-    },
-    {
-      date: new Date("2025-02-07T12:00:00"),
-      description: "Recebimento salário",
-      amount: 2700,
-      categoryId: "a",
-    },
-  ];
+// Data
+const transactions: ITransaction[] = [
+  {
+    id: "z",
+    date: new Date("2025-02-12T12:00:00"),
+    description: "Compras diversas",
+    amount: 210,
+    categoryId: "c",
+  },
+  {
+    id: "y",
+    date: new Date("2025-02-11T12:00:00"),
+    description: "Pgto. Aluguel Casa",
+    amount: 2600,
+    categoryId: "d",
+  },
+  {
+    id: "x",
+    date: new Date("2025-02-09T12:00:00"),
+    description: "Lucro com ações",
+    amount: 480,
+    categoryId: "b",
+  },
+  {
+    id: "w",
+    date: new Date("2025-02-08T12:00:00"),
+    description: "Pgto. Agua e Luz",
+    amount: 370,
+    categoryId: "d",
+  },
+  {
+    id: "t",
+    date: new Date("2025-02-07T12:00:00"),
+    description: "Recebimento salário",
+    amount: 2700,
+    categoryId: "a",
+  },
+];
 
+const categories: ICategory[] = [
+  {
+    id: "a",
+    name: "Salário",
+    type: "INCOME",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
+  },
+  {
+    id: "b",
+    name: "Outras Receitas",
+    type: "INCOME",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+  },
+  {
+    id: "c",
+    name: "Despesas Variáveis",
+    type: "EXPENSE",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+  },
+  {
+    id: "d",
+    name: "Despesas Obrigatórias",
+    type: "EXPENSE",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 16,
+  },
+];
+
+const categoriesForDashboard: TCategoryForDashboard[] = [
+  {
+    id: "a",
+    name: "Salário",
+    total: 2700,
+    type: "INCOME",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
+  },
+  {
+    id: "d",
+    name: "Despesas Obrigatórias",
+    total: 2970,
+    type: "EXPENSE",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 16,
+  },
+  {
+    id: "b",
+    name: "Outras Receitas",
+    total: 480,
+    type: "INCOME",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+  },
+  {
+    id: "c",
+    name: "Despesas Variáveis",
+    total: 210,
+    type: "EXPENSE",
+    updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+  },
+];
+
+/**
+ * API Functions Area
+ */
+export const listTransactions: TListTransactionsFunction = async () => {
   transactions.sort((a, b) => b.date.getTime() - a.date.getTime());
 
   return new Promise((resolve) => {
@@ -65,41 +134,25 @@ export const listTransactions: TListTransactionsFunction = async () => {
   });
 };
 
+export const getTransactionById: TGetTransactionFunction = async (id) => {
+  const transaction = transactions.find((t) => t.id === id);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(transaction || null);
+    }, 200);
+  });
+};
+
+export const listCategories: TListCategoriesFunction = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(categories), 300);
+  });
+};
+
 export const listCategoriesForDashboard: TListCategoriesDashboardFunction =
   async () => {
     return new Promise((resolve) => {
-      const categories: TCategoryForDashboard[] = [
-        {
-          id: "a",
-          name: "Salário",
-          total: 2700,
-          type: "INCOME",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
-        },
-        {
-          id: "d",
-          name: "Despesas Obrigatórias",
-          total: 2970,
-          type: "EXPENSE",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 16,
-        },
-        {
-          id: "b",
-          name: "Outras Receitas",
-          total: 480,
-          type: "INCOME",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
-        },
-        {
-          id: "c",
-          name: "Despesas Variáveis",
-          total: 210,
-          type: "EXPENSE",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
-        },
-      ];
-
-      categories.sort((a, b) => {
+      categoriesForDashboard.sort((a, b) => {
         if (a.type !== b.type) {
           return a.type === "INCOME" ? -1 : 1;
         }
@@ -108,44 +161,14 @@ export const listCategoriesForDashboard: TListCategoriesDashboardFunction =
       });
 
       setTimeout(() => {
-        resolve(categories);
+        resolve(categoriesForDashboard);
       }, 300);
     });
   };
 
-export const listCategories: TListCategoriesFunction = async () => {
-  const categories: ICategory[] = [
-    {
-      id: "a",
-      name: "Salário",
-      type: "INCOME",
-      updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
-    },
-    {
-      id: "b",
-      name: "Outras Receitas",
-      type: "INCOME",
-      updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
-    },
-    {
-      id: "c",
-      name: "Despesas Variáveis",
-      type: "EXPENSE",
-      updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
-    },
-    {
-      id: "d",
-      name: "Despesas Obrigatórias",
-      type: "EXPENSE",
-      updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 16,
-    },
-  ];
+export const createTransaction = async (data: Partial<ITransaction>) => {};
 
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(categories), 300);
-  });
-};
-
-export const createTransaction = async (data: ITransaction) => {};
-
-export const editTransaction = async (id: string, data: ITransaction) => {};
+export const editTransaction = async (
+  id: string,
+  data: Partial<ITransaction>
+) => {};
