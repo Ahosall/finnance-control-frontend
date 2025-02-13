e7debbd0787566387b8b34b57a9530f1e4023795
Ahosall <zen.wixyos.br@gmail.com>
2025-02-12 22:50:38 -0400

    add function to list transactions, correct values to fixed values, improve data processing logic

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@
  */
 
 export interface ITransaction {
-  date: string;
+  date: Date;
   categoryId: string;
   amount: number;
   description: string;
@@ -17,53 +17,94 @@ export interface ICategory {
   updatedAt: number;
 }
 
-type TCategoriesForDashboardFunction = () => Promise<ICategory[]>;
+type TListCategoriesDashboardFunction = () => Promise<ICategory[]>;
+type TListTransactionsFunction = () => Promise<ITransaction[]>;
 
-export const listCategoriesForDashboard: TCategoriesForDashboardFunction =
+export const listTransactions: TListTransactionsFunction = async () => {
+  const transactions: ITransaction[] = [
+    {
+      date: new Date("2025-02-12T12:00:00"),
+      description: "Compras diversas",
+      amount: 210,
+      categoryId: "c",
+    },
+    {
+      date: new Date("2025-02-11T12:00:00"),
+      description: "Pgto. Aluguel Casa",
+      amount: 2600,
+      categoryId: "d",
+    },
+    {
+      date: new Date("2025-02-09T12:00:00"),
+      description: "Lucro com ações",
+      amount: 480,
+      categoryId: "b",
+    },
+    {
+      date: new Date("2025-02-08T12:00:00"),
+      description: "Pgto. Agua e Luz",
+      amount: 370,
+      categoryId: "d",
+    },
+    {
+      date: new Date("2025-02-07T12:00:00"),
+      description: "Recebimento salário",
+      amount: 2700,
+      categoryId: "a",
+    },
+  ];
+
+  transactions.sort((a, b) => b.date.getTime() - a.date.getTime());
+
+  return new Promise((resolve) => {
+    resolve(transactions);
+  });
+};
+
+export const listCategoriesForDashboard: TListCategoriesDashboardFunction =
   async () => {
     return new Promise((resolve) => {
-      const transactions: ICategory[] = [
+      const categories: ICategory[] = [
         {
           id: "a",
           name: "Salário",
-          total: Math.random() * 3000,
+          total: 2700,
           type: "INCOME",
           updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
         },
         {
           id: "d",
           name: "Despesas Obrigatórias",
-          total: Math.random() * 1000,
+          total: 2970,
           type: "EXPENSE",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 16,
         },
         {
           id: "b",
           name: "Outras Receitas",
-          total: Math.random() * 50,
+          total: 480,
           type: "INCOME",
           updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
         },
         {
           id: "c",
           name: "Despesas Variáveis",
-          total: Math.random() * 500,
+          total: 210,
           type: "EXPENSE",
-          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 12,
+          updatedAt: Date.now() - 1000 * 60 * 60 * 24 * 10,
         },
       ];
 
-      transactions.sort((a, b) => {
+      categories.sort((a, b) => {
         if (a.type !== b.type) {
           return a.type === "INCOME" ? -1 : 1;
         }
 
-        // Se forem do mesmo tipo, ordena por data (mais recente primeiro)
         return a.updatedAt - b.updatedAt;
       });
 
       setTimeout(() => {
-        resolve(transactions);
+        resolve(categories);
       }, 300);
     });
   };
