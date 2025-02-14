9f0a0e67ef47e99e79629c797318c0f8cbb0d038
Ahosall <zen.wixyos.br@gmail.com>
2025-02-14 19:17:07 -0400

    include application in authentication module

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 import App from "./App.tsx";
+
+import { AuthProvider } from "./context/auth.context.tsx";
+import { darkTheme } from "./theme.tsx";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,6 +14,11 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
