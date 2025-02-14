8afc36e2805c8eb968f465ceee0a588073991ba8
Ahosall <zen.wixyos.br@gmail.com>
2025-02-14 19:18:15 -0400

    create authentication routes, and protect

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../context/auth.context";
+
+interface Props {
+  children: ReactNode;
+}
+
+const PrivateRoute = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const { token } = useAuth();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/signin");
+    }
+  }, [token]);
+
+  return token === null ? <></> : <>{children}</>;
+};
+
+export default PrivateRoute;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,45 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import PrivateRoute from "./components/PrivateRoute";
 
 import Dashboard from "./pages/Dashboard";
+import NewCategory from "./pages/NewCategory";
 
+// Transaction Pages
 import NewTransaction from "./pages/transactions/New";
 import ListTransactions from "./pages/transactions/List";
 import ViewTransaction from "./pages/transactions/View";
 import EditTransaction from "./pages/transactions/Edit";
 
-import NewCategory from "./pages/NewCategory";
+// Auth pages
+import SignIn from "./pages/Auth/Signin";
+import SignUp from "./pages/Auth/Signup";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <PrivateRoute>
+        <Layout />
+      </PrivateRoute>
+    ),
     children: [
       { path: "", element: <Dashboard /> },
+      { path: "/new-category", element: <NewCategory /> },
       { path: "/transactions/", element: <ListTransactions /> },
       { path: "/transactions/new", element: <NewTransaction /> },
       { path: "/transactions/:id", element: <ViewTransaction /> },
       { path: "/transactions/:id/edit", element: <EditTransaction /> },
-      { path: "/new-category", element: <NewCategory /> },
     ],
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
 
